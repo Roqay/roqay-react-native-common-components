851a677594f2c2e8af414ed7dbe16d5238232697
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2022-12-25 11:30:38 +0200

    feat: support new `Card` props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,7 +13,7 @@ import type {
 const styles = ScaledSheet.create({
   card: {
     width: '95%',
-    alignSelf: 'center',
+    marginHorizontal: '2.5%',
     padding: '8@msr',
     borderRadius: '8@msr',
     marginVertical: '4@msr',
@@ -23,35 +23,14 @@ const styles = ScaledSheet.create({
 
 // #region Types
 export type Props = React.ComponentProps<typeof PaperCard>;
-
 type PropsWithTheme = Props & {
   theme: MD2Theme | MD3Theme;
 };
 // #endregion
 
 const Card = (props: PropsWithTheme): React.ReactElement => {
-  const { mode, elevation, style, ...other } = props;
-
-  switch (mode) {
-    case 'outlined':
-      return (
-        <PaperCard
-          mode="outlined"
-          elevation={undefined}
-          style={[styles.card, style]}
-          {...other}
-        />
-      );
-    default:
-      return (
-        <PaperCard
-          mode="elevated"
-          elevation={elevation === undefined ? 2 : elevation}
-          style={[styles.card, style]}
-          {...other}
-        />
-      );
-  }
+  const { style, ...other } = props;
+  return <PaperCard style={[styles.card, style]} {...other} />;
 };
 
 export default withTheme(Card);
